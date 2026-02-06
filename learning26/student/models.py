2e703a60e07623ac917154ce2bb1eece927046f5
Keshav Pancholi <keshavpancholi0611@gmail.com>
2026-02-06 11:33:01 +0530

    Created three modules: StudentProfile, Category & Service in Student application - On 6th Feb 2026

--- learning26/student/models.py
+++ learning26/student/models.py
@@ -16,6 +16,9 @@ class Student(models.Model):
     class Meta:
         db_table = "student"  #table name
 
+    def __str__(self):
+        return self.studentName
+
 class Product(models.Model):
     productName = models.CharField(max_length=100)
     productPrice = models.IntegerField()
@@ -26,5 +29,46 @@ class Product(models.Model):
     
     class Meta:
         db_table = "product"
-        
+       
+class StudentProfile(models.Model):
+    hobbies = (("reading","reading"),("travel","travel"),("music","music"))
+    studentID = models.OneToOneField(Student, on_delete=models.CASCADE)
+    studentHobbies = models.CharField(max_length=100,choices=hobbies)
+    studentAddress = models.CharField(max_length=100)
+    studentPhone = models.CharField(max_length=10)
+    studentGender = models.CharField(max_length=10)
+    studentDOB = models.DateField()
+    
+    class Meta:
+        db_table = "studentprofile"
 
+    def __str__(self):
+        return self.studentID.studentName
+
+#cat --> #service
+
+class Category(models.Model):
+    categoryName = models.CharField(max_length=100)
+    categoryDescription = models.TextField()
+    categoryStatus = models.BooleanField(default=True)
+    
+    class Meta:
+        db_table = "category"
+
+    def __str__(self):
+        return self.categoryName
+
+class Service(models.Model):
+    serviceName = models.CharField(max_length=100)
+    serviceDescription = models.TextField()
+    servicePrice = models.IntegerField()
+    serviceStatus = models.BooleanField(default=True)
+    #after table creation adding new field
+    discount = models.IntegerField(null=True)
+    categoryId = models.ForeignKey(Category,on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "service"
+
+    def __str__(self):
+        return self.serviceName 
