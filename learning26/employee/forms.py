b030361b56a8c745aa884c5c55b3bfb5f21077c1
Keshav Pancholi <keshavpancholi0611@gmail.com>
2026-02-12 22:42:27 +0530

    Learnt concept name:

--- learning26/employee/forms.py
+++ learning26/employee/forms.py
@@ -12,3 +12,4 @@ class CourseForm(forms.ModelForm):
     class Meta:
         model = Course
         fields = '__all__' 
+
--- learning26/employee/urls.py
+++ learning26/employee/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('employeeList/', views.employeeList),
+    path('employeeList/', views.employeeList,name="employeeList"),
     path('employeeFilter/', views.employeeFilter),
     path('createemployee/',views.createEmployee),
-    path('createEmployeeWithForm/',views.createEmployeeWithForm),
-    path('createCourse/',views.createCourse)
+    path('createEmployeeWithForm/',views.createEmployeeWithForm,name="createEmployeeWithForm"),
+    path('createCourse/',views.createCourse),
+    #path('deleteEmployee/',views.deleteEmployee,name="deleteEmployee")
+    path("deleteEmployee/<int:id>",views.deleteEmployee,name="deleteEmployee"),
+    path("filterEmployee/",views.filterEmployee,name="filterEmployee"),
 ]
