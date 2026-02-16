07afa861c2077ab9549ae195c43da4e90a12c47e
Keshav Pancholi <keshavpancholi0611@gmail.com>
2026-02-16 23:12:04 +0530

    UPDATE & DELETE operations performed in 'services' application - 13th Feb's TASK completed

--- learning26/services/urls.py
+++ learning26/services/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('serviceList/', views.serviceList,name="serviceList"),
-    path('createService/',views.createService,name="createService")
+    path('createService/',views.createService,name="createService"),
+    path("deleteService/<int:id>",views.deleteService,name="deleteService"),
+    path("updateService/<int:id>",views.updateService,name="updateService")
 ]
--- learning26/services/views.py
+++ learning26/services/views.py
@@ -4,7 +4,7 @@ from .forms import ServiceForm
 
 # Create your views here.
 def serviceList(request):
-    services = Service.objects.all()
+    services = Service.objects.all().order_by("id").values()
     return render(request, 'services/serviceList.html', {"services":services})
 
 def createService(request):
@@ -21,3 +21,24 @@ def createService(request):
         form = ServiceForm() #form object    
 
     return render(request,"services/createService.html",{"form":form})
+
+def deleteService(request,id):
+    #delete from services where id = 1
+    print("id from url = ",id)
+    Service.objects.filter(id=id).delete()
+    #return HttpResponse("SERVICE DELETED SUCCESSFULLY...")
+    #employee list redirecr
+    return redirect("serviceList") #url --> name -->
+
+#update --->
+def updateService(request,id):
+    #database existing user... id -->
+    service = Service.objects.get(id=id) #select * from service where id = 1
+    
+    if request.method == "POST":
+        form = ServiceForm(request.POST,instance=service)
+        form.save()
+        return redirect("serviceList")
+    else:
+        form = ServiceForm(instance=service)    
+        return render(request,"services/updateService.html",{"form":form})
